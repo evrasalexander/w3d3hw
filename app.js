ac384b8a34bcf9fb45b317ec91b85af434de5778
unknown <evrasalexander@gmail.com>
2022-01-19 19:22:22 -0500

    homework

---
+++ app.js
@@ -0,0 +1,23 @@
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+var j = 10;
+
+console.log(a + j);
+console.log(b + i);
+console.log(c - h);
+console.log(d - g);
+console.log(e * f)
+console.log(a * j)
+console.log(b / i)
+console.log(c / h)
+console.log(d++)
+console.log(e++)
+console.log(g--)
+console.log(f--)
